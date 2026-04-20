190e59c8e52d1dbf3d96f9e8760e3916e082bdc6
augusto <edgaraah126@gmail.com>
2026-04-20 06:41:54 -0500

    Agregando nueva estructura de control: Bucles

---
+++ Bucle.py
@@ -0,0 +1,19 @@
+'''
+En programación podemos definir un bucle como la repetición de la ejecución de un conjunto
+de instrucciones donde a cada repetición del conjunto de instrucciones se llama iteracción.
+
+Los Bucles están compuestos por los siguientes elementos:
+──▶ Punto de inicio: Es el punto donde se inicia la ejecución del bucle.
+──▶ Punto de fin: Es el punto donde se detiene la ejecución del bucle.
+──▶ Número de iteracciones: Es el número de veces que se ejecutará el conjunto de instrucciones.
+──▶ Bloque de instrucciones: Es el conjunto de instrucciones que se ejecutará en cada iteracción del bucle.
+
+Existen dos tipos de bucles:
+──▶ Bucle for: Es un bucle que se ejecuta un número determinado de veces. 
+               Se utiliza cuando se conoce el número de iteracciones que se van a realizar.
+──▶ Bucle while: Es un bucle que se ejecuta mientras se cumpla una condición. 
+ Se utiliza cuando no se conoce el número de iteracciones que se van a realizar, 
+ pero se sabe que se desea repetir un bloque de instrucciones mientras se cumpla una condición.    
+
+ 
+'''
